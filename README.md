ad8c927965a48a8a8f7e948bd4cf74632fa00afe
Andrey_B <example@gmail.com>
2022-03-22 14:23:53 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Next.js + Tailwind CSS nft marketplace app
+
+## 
+```
+Making nft's marketplace on ethereum testnet Rinkeby
+```
+
+
+
+
+```
+Using [NextJS], [Tailwind].
+Using [Metamask], [Thirdweb] SDK and api for marketplace and nft collection
+Using [Sanity] as database
+```
---
+++ pages/nfts/[nftId].jsx
@@ -0,0 +1,89 @@
+import { useEffect, useMemo, useState } from 'react'
+import { useWeb3 } from '@3rdweb/hooks'
+import { ThirdwebSDK } from '@3rdweb/sdk'
+import { useRouter } from 'next/router'
+import Header from '../../components/Header'
+import NFTImage from '../../components/nft/NFTImage'
+import GeneralDetails from '../../components/nft/GeneralDetails'
+import Purchase from '../../components/nft/Purchase'
+import ItemActivity from '../../components/nft/ItemActivity'
+
+const style = {
+  wrapper: `flex flex-col items-center justify-center  text-[#e5e8eb]`,
+  container: `flex flex-col p-6`,
+  topContent: `flex`,
+  nftImgContainer: `flex-1 mr-4`,
+  detailsContainer: `flex-[2] ml-4`,
+}
+
+const nftId = () => {
+  const { provider } = useWeb3()
+  const [selectedNft, setSelectedNft] = useState()
+  const [listings, setListings] = useState([])
+  const router = useRouter()
+
+  const nftModule = useMemo(() => {
+    if (!provider) return
+
+    const sdk = new ThirdwebSDK(
+      provider.getSigner()
+    )
+    return sdk.getNFTModule('0x34F18259176de309906b705B6Bf8462D8B9dA988')
+  }, [provider])
+
+  // get all NFTs in the collection
+  useEffect(() => {
+    if (!nftModule) return
+    ;(async () => {
+      const nfts = await nftModule.getAll()
+      const selectedNftItem = nfts.find((nft) => nft.id === router.query.nftId)
+      setSelectedNft(selectedNftItem)
+    })()
+  }, [nftModule])
+
+  const marketPlaceModule = useMemo(() => {
+    if (!provider) return
+
+    const sdk = new ThirdwebSDK(
+      provider.getSigner()
+    )
+    return sdk.getMarketplaceModule(
+      '0x896069138640AA79F068C4962fcf85d5941bae16'
+    )
+  }, [provider])
+
+  // get all listings in the collection
+  useEffect(() => {
+    if (!marketPlaceModule) return
+    ;(async () => {
+      setListings(await marketPlaceModule.getAllListings())
+    })()
+  }, [marketPlaceModule])
+
+  return (
+    <div>
+      <Header />
+      <div className={style.wrapper}>
+        <div className={style.container}>
+          <div className={style.topContent}>
+            <div className={style.nftImgContainer}>
+              <NFTImage selectedNft={selectedNft} />
+            </div>
+            <div className={style.detailsContainer}>
+              <GeneralDetails selectedNft={selectedNft} />
+              <Purchase
+                isListed={router.query.isListed}
+                selectedNft={selectedNft}
+                listings={listings}
+                marketPlaceModule={marketPlaceModule}
+              />
+            </div>
+          </div>
+          <ItemActivity />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default nftId
